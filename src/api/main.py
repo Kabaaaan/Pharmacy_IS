a7f5a6cdcafb0025d360c8777c5b1113c8b7c49e
Kabaan <romanfcb123@gmail.ru>
2025-05-30 00:28:55 +0300

    хотфикс порта

--- src/api/main.py
+++ src/api/main.py
@@ -33,4 +33,4 @@ async def index():
 
 if __name__ == "__main__":
     init_db()
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
